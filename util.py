134b594c25cab4354fbfb069f1f937517022edc6
Eric Kingery <ekingery@gmail.com>
2017-11-30 20:35:47 -0600

    adjusting time switch to noon

--- util.py
+++ util.py
@@ -9,9 +9,9 @@ def date_to_display(current_dt):
     day_index = current_dt.weekday()  # Monday = 0, Sunday = 6
     hour = current_dt.hour
 
-    # Test for after 11am on Friday, all weekend, and Monday before 11am
+    # Test for after noon on Friday, all weekend, and Monday before noon
     if day_index in [FRIDAY, SATURDAY, SUNDAY, MONDAY]:
-        if FRIDAY == day_index and hour > 10:
+        if FRIDAY == day_index and hour > 11:
             # Add three days to get Monday
             return current_dt + timedelta(3)
         if SATURDAY == day_index:
@@ -20,13 +20,13 @@ def date_to_display(current_dt):
         if SUNDAY == day_index:
             # add one day to get Monday
             return current_dt + timedelta(1)
-        if MONDAY == day_index and hour <= 10:
+        if MONDAY == day_index and hour <= 11:
             return current_dt
 
-    # At this point, we can simply test for before 11am
-    if hour <= 10:
-        # and return the current day if it's before 11am
+    # At this point, we can simply test for before noon
+    if hour <= 11:
+        # and return the current day if it's before noon
         return current_dt
 
-    # It is after 11am on a weekday, return the next day
+    # It is after noon on a weekday, return the next day
     return current_dt + timedelta(1)
