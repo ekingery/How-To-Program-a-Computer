4dc023beb005ef5a653f2220562fe62f02260cec
Michael S. Manley <msm@manley.org>
2018-01-09 14:55:55 -0600

    rearrange to prep for weather api key check

--- smines_city/views.py
+++ smines_city/views.py
@@ -2,6 +2,7 @@
 from flask import render_template, send_from_directory
 # load modules for dealing with dates and times
 from datetime import datetime
+import os
 import pytz  # timezones
 
 # import the app so we can register routes to serve
@@ -15,11 +16,15 @@ from smines_city.weather import get_weather
 # Serve a web page at the root of the URL
 @app.route('/')
 def homepage():
+    # check for the one and only confg var we need
+
+    
     # This is the main function that serves the web page
     current_dt = datetime.now().astimezone(tz=pytz.timezone('US/Central'))
     display_date = date_to_display(current_dt)
     formatted_date = display_date.strftime("%A, %B %d")
-    items = scrape_menu(current_dt)
+
+    # check the weather first
     forecast = get_weather(display_date)
     current = forecast.currently()
     hourly = forecast.hourly()
@@ -32,6 +37,9 @@ def homepage():
     }
     summary = 'Currently ' + current.summary + '. ' + \
         hourly.summary + ' ' + daily.summary
+
+    # get the menu for today
+    items = scrape_menu(current_dt)
 
     # Return the html markup we use to serve the page
     return render_template(
