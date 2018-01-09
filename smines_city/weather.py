794c1482cd2e54bf02b6f67c80b4d463c940dacc
Michael S. Manley <msm@manley.org>
2018-01-09 14:56:42 -0600

    make sure api key is present

--- smines_city/weather.py
+++ smines_city/weather.py
@@ -5,6 +5,8 @@ import forecastio
 def get_weather(date_to_display):
     # Dark Sky API Key
     api_key = os.environ.get('DARKSKY_API_KEY')
+    if api_key is None
+        return None
     lat = 41.9030703
     lon = -87.8006613
     forecast = forecastio.load_forecast(api_key, lat, lon)
